55f650d906573ea0afe1dd09e8dae292f7b2ba1c
Lupeni Tuikalepa <lupeni.tuikalepa@gmail.com>
2024-09-23 19:18:51 +0200

    scene and folders note

---
+++ Editor/Annotations/AssetsAnnotation/LTXAssetData.cs
@@ -0,0 +1,17 @@
+﻿using LTX.Tools;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace LTX.Editor.Annotations.FoldersAnnotation
+{
+    [System.Serializable]
+    public struct LTXAssetData
+    {
+        [SerializeField]
+        public Annotable annotation;
+
+        [SerializeField]
+        public string path;
+
+    }
+}
